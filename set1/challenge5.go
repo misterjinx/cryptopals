6844449eb55fe3e948b52d6be2d2183580645482
Marius <marius@freshcolors.ro>
2017-11-26 17:16:52 +0200

    Add set 1 challenge 5

---
+++ set1/challenge5.go
@@ -0,0 +1,14 @@
+package set1
+
+func repeatingKeyXOR(text string, key string) []byte {
+	textLength := len(text)
+	keyLength := len(key)
+
+	xored := make([]byte, textLength)
+
+	for i := range text {
+		xored[i] = text[i] ^ key[i%keyLength]
+	}
+
+	return xored
+}
